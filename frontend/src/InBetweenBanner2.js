a4937abde0de316253f4454e734a06c86b193638
Fathima-ashfaq <110375534+Fathima-ashfaq@users.noreply.github.com>
2022-09-05 16:20:14 +0530

    initial commit

---
+++ frontend/src/InBetweenBanner2.js
@@ -0,0 +1,17 @@
+import rec from "./images/image1.png";
+import "./style.css";
+
+export default function InBetweenBanner2(){
+    return(
+        <>
+          <div className="InBetweenBanner2" style={{backgroundImage: `url(${rec})`}}>
+            <div className="InBetweenBanner2_label1">Get Latest Update By Subscribe</div>
+            <div className="InBetweenBanner2_label2">Our Newslater</div>
+            <div className="InBetweenBanner2_button">
+                <button>Shop Now</button>
+            </div>
+             
+          </div>
+        </>
+    );
+}
---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import InBetweenBanner2 from './InBetweenBanner2';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <InBetweenBanner2/>
+  </React.StrictMode>
+);
+
+
