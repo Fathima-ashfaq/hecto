f705b8d1c220694477663bc65ea737bbb3f5991a
Fathima-ashfaq <110375534+Fathima-ashfaq@users.noreply.github.com>
2022-09-21 19:55:39 +0530

    Done

--- frontend/src/Adminsubtitle.js
+++ frontend/src/Adminsubtitle.js
@@ -1,5 +1,5 @@
 import { BsFilter } from "react-icons/bs";
-import { TiTickOutline } from "react-icons/ti";
+import { TiTick } from "react-icons/ti";
 import { BsSearch } from "react-icons/bs";
 import { MdModeEditOutline } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
@@ -14,51 +14,41 @@ export default function Adminsubtitle() {
                 <div className="adminsubtitle_left">
                     <div className="adminsubtitle_left_icon">
                         {/* <BsEye style={{color:'red', fontSize:50}}/> */}
-                        <TiTickOutline style={{ color: ' #F5F5F5', fontSize: 20 }} />
+                        <TiTick style={{ color: ' #F5F5F5', fontSize: 20 }} />
                     </div>
                     <div className="adminsubtitle_left_label">
                         <label>Select All</label>
                     </div>
                 </div>
                 <div className="adminsubtitle_center">
-                    <ul>
-                        <li>
-                            <div className="adminsubtitle_center_list">
-                                <BsFilter />
-                                <label>Filters</label>
-                            </div>
+                    <div className="adminsubtitle_center_list1">
+                        <BsFilter  style={{fontSize:20}}/>
+                        <label>Filters</label>
+                    </div>
+                    <div className="adminsubtitle_center_list">
+                        <BsSearch />
+                        <label>Search</label>
+                    </div>
+                    <div className="adminsubtitle_center_list">
+                        <MdModeEditOutline style={{fontSize:20}}/>
+                        <label>Edit</label>
+                    </div>
 
-                        </li>
-                        <li>
-                            <div className="adminsubtitle_center_list">
-                                <BsSearch />
-                                <label>Search</label>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="adminsubtitle_center_list">
-                                <MdModeEditOutline />
-                                <label>Edit</label>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="adminsubtitle_center_list">
-                                <MdDelete />
-                                <label>Delete</label>
-                            </div>
-                        </li>
-                    </ul>
+                    <div className="adminsubtitle_center_list2">
+                        <MdDelete style={{fontSize:20}} />
+                        <label>Delete</label>
+                    </div>        
                 </div>
                 <div className="adminsubtitle_right">
 
                     <div className="adminsubtitle_right_list1">
-                        <BsJustify />
+                        <BsJustify  style={{fontSize:20}}/>
                     </div>
                     <div className="adminsubtitle_right_list2">
-                        <BsFillGrid3X3GapFill />
+                        <BsFillGrid3X3GapFill  style={{fontSize:20}}/>
                     </div>
                     <div className="adminsubtitle_right_list3">
-                        <FaThList />
+                        <FaThList  style={{fontSize:20}}/>
                     </div>
 
                 </div>
