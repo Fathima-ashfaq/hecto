67d4f8d748ae91f1121e50780e5bf08e67e236a8
Fathima-ashfaq <110375534+Fathima-ashfaq@users.noreply.github.com>
2022-09-30 15:53:01 +0530

    Done

--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -18,7 +18,7 @@ import Adminproductreview from "./Adminproductreview";
 import Adminproductreviewbar from "./Adminproductreviewbar";
 import Adminstatisticsgraph from "./Adminstatisticsgraph";
 import Adminproductimagesummary from "./Adminproductimagesummary";
-
+import Profileedit from "./Profileedit";
 export default function Navigation(){
     return(
         <>
@@ -42,7 +42,7 @@ export default function Navigation(){
                 <Route path="/Adminproductreviewbar" element={<Adminproductreviewbar/>}></Route>
                 <Route path="/Adminstatisticsgraph" element={<Adminstatisticsgraph/>}></Route>
                 <Route path="/Adminproductimagesummary" element={<Adminproductimagesummary/>}></Route>
-               
+                <Route path="/Profileedit" element={<Profileedit/>}></Route>
             </Routes> 
         </HashRouter>
 
